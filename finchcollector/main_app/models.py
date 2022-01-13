7c273e3fc82265c3f9e46b8d3171e7b436ae58dd
mrjohnbeard <wengerbeard@gmail.com>
2022-01-13 10:59:22 -0800

    add Finch class/model

--- finchcollector/main_app/models.py
+++ finchcollector/main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Finch: 
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('Andy', '01Breed', 'kinda red', 20),
+  Finch('Bandy', '02Breed', 'kinda blue', 25),
+  Finch('Candy', '03Breed', 'kinda orange', 30),
+  Finch('Dandy', '04Breed', 'kinda green', 35)
+]
