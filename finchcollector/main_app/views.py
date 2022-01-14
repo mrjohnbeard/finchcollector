a8ada548fa71232049cd3c5dd9cac93a4ff88a9a
mrjohnbeard <wengerbeard@gmail.com>
2022-01-14 03:16:24 -0800

    add templates and views for delete edit

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # Create your views here.
@@ -21,4 +21,12 @@ def finches_detail(request, finch_id):
 class FinchCreate(CreateView):
   model = Finch
   fields = '__all__'
+  success_url = '/finches/'
+  
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['color', 'occupation', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
   success_url = '/finches/'
