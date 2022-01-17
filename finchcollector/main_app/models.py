3483f6b35db9073d4568d3e3888409bf7b17a024
mrjohnbeard <wengerbeard@gmail.com>
2022-01-17 15:16:19 -0800

    add toy model

--- finchcollector/main_app/models.py
+++ finchcollector/main_app/models.py
@@ -9,6 +9,17 @@ MEALS = (
   ('D', 'Dinner')
 )
 
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+
 class Finch(models.Model):
   name = models.CharField(max_length=150)
   color = models.CharField(max_length=150)
