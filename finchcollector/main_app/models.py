91271e24ee851854490d5b899e7e5dbc43e40e5b
mrjohnbeard <wengerbeard@gmail.com>
2022-01-17 14:58:06 -0800

    add feeding

--- finchcollector/main_app/models.py
+++ finchcollector/main_app/models.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 
 # Create your models here.
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 class Finch(models.Model):
   name = models.CharField(max_length=150)
   color = models.CharField(max_length=150)
@@ -14,3 +20,16 @@ class Finch(models.Model):
   
   def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+      
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    
+    
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
